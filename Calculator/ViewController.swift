32c6d584b8a388f55e4b25c729f2d330f4f0f088
Hugo Argueta <hugoargueta94@gmail.com>
2016-09-07 15:56:21 -0700

    Added features specified in assignment 1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,14 +11,24 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var userHistory: UILabel!
     
     var userIsInTheMiddleOfTypingANumber = false
+    var didPerformOperation = false
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTypingANumber {
-            display.text = display.text! + digit
+            if digit == "." {
+                if !display.text!.containsString(".") {
+                    display.text = display.text! + digit
+                }
+            } else {
+                display.text = display.text! + digit
+            }
         } else {
+            if digit != "." {
+            }
             display.text = digit
             userIsInTheMiddleOfTypingANumber = true
         }
@@ -31,13 +41,19 @@ class ViewController: UIViewController {
             enter()
         }
         
+        userHistory.text = userHistory.text! + " " + operation
+        
         switch operation {
             case "✕": performOperation {$0 * $1}
             case "÷": performOperation {$1 / $0}
             case "+": performOperation {$0 + $1}
             case "-": performOperation {$1 - $0}
             case "√": performOperation {sqrt($0)}
-            
+            case "sin": performOperation { sin($0) }
+            case "cos": performOperation { cos($0) }
+            case "π":
+                displayValue = M_PI
+                enter()
             default:
                 break
         }
@@ -46,14 +62,18 @@ class ViewController: UIViewController {
     func performOperation(operation: (Double, Double) -> Double) {
         if operandStack.count >= 2 {
             displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            didPerformOperation = true
             enter()
+            didPerformOperation = false
         }
     }
     
     private func performOperation(operation: Double -> Double) {
         if operandStack.count >= 1 {
             displayValue = operation(operandStack.removeLast())
+            didPerformOperation = true
             enter()
+            didPerformOperation = false
         }
     }
     
@@ -63,6 +83,9 @@ class ViewController: UIViewController {
     @IBAction func enter() {
         userIsInTheMiddleOfTypingANumber = false
         operandStack.append(displayValue)
+        if !didPerformOperation {
+            userHistory.text = userHistory.text! + " " + "\(displayValue)"
+        }
         print("Operand Stack = \(operandStack)")
     }
     
@@ -80,6 +103,7 @@ class ViewController: UIViewController {
     @IBAction func clear() {
         operandStack.removeAll()
         display.text = "\(0)"
+        userHistory.text = ""
         userIsInTheMiddleOfTypingANumber = false
     }
 }
