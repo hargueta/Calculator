af90f32349438328677abdc47c4c1f3bdd7b25ce
Hugo Argueta <hugoargueta94@gmail.com>
2016-09-12 17:40:55 -0700

    Added the code from Lecture #3

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     @IBOutlet weak var userHistory: UILabel!
     
+    var brain = CalculatorBrain()
+    
     var userIsInTheMiddleOfTypingANumber = false
     var didPerformOperation = false
     
@@ -35,58 +37,74 @@ class ViewController: UIViewController {
     }
     
     @IBAction func operate(sender: UIButton) {
-        let operation = sender.currentTitle!
+//        let operation = sender.currentTitle!
         
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
         
-        userHistory.text = userHistory.text! + " " + operation
+        if let operation = sender.currentTitle {
+            userHistory.text = userHistory.text! + " " + operation
+            
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
         
-        switch operation {
-            case "✕": performOperation {$0 * $1}
-            case "÷": performOperation {$1 / $0}
-            case "+": performOperation {$0 + $1}
-            case "-": performOperation {$1 - $0}
-            case "√": performOperation {sqrt($0)}
-            case "sin": performOperation { sin($0) }
-            case "cos": performOperation { cos($0) }
-            case "π":
-                displayValue = M_PI
-                enter()
-            default:
-                break
-        }
+        
+        
+//        switch operation {
+//            case "✕": performOperation {$0 * $1}
+//            case "÷": performOperation {$1 / $0}
+//            case "+": performOperation {$0 + $1}
+//            case "-": performOperation {$1 - $0}
+//            case "√": performOperation {sqrt($0)}
+//            case "sin": performOperation { sin($0) }
+//            case "cos": performOperation { cos($0) }
+//            case "π":
+//                displayValue = M_PI
+//                enter()
+//            default:
+//                break
+//        }
     }
     
-    func performOperation(operation: (Double, Double) -> Double) {
-        if operandStack.count >= 2 {
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            didPerformOperation = true
-            enter()
-            didPerformOperation = false
-        }
-    }
-    
-    private func performOperation(operation: Double -> Double) {
-        if operandStack.count >= 1 {
-            displayValue = operation(operandStack.removeLast())
-            didPerformOperation = true
-            enter()
-            didPerformOperation = false
-        }
-    }
+//    func performOperation(operation: (Double, Double) -> Double) {
+//        if operandStack.count >= 2 {
+//            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+//            didPerformOperation = true
+//            enter()
+//            didPerformOperation = false
+//        }
+//    }
+//    
+//    private func performOperation(operation: Double -> Double) {
+//        if operandStack.count >= 1 {
+//            displayValue = operation(operandStack.removeLast())
+//            didPerformOperation = true
+//            enter()
+//            didPerformOperation = false
+//        }
+//    }
     
     
     var operandStack = Array<Double>()
     
     @IBAction func enter() {
         userIsInTheMiddleOfTypingANumber = false
-        operandStack.append(displayValue)
+//        operandStack.append(displayValue)
         if !didPerformOperation {
             userHistory.text = userHistory.text! + " " + "\(displayValue)"
         }
-        print("Operand Stack = \(operandStack)")
+        
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+//        print("Operand Stack = \(operandStack)")
     }
     
     var displayValue: Double {
